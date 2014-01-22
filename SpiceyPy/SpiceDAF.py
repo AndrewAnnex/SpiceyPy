3482d3cd593f07c402672a93ca891d6c483cb424
Andrew Annex <ama6fy@virginia.edu>
2014-01-22 14:51:39 -0500

    Start of restructure, it turns out too many low level functions exist to be practical to include in a single large file (spicecommands.py). Also reading binary kernels is of top importance.

---
+++ SpiceyPy/SpiceDAF.py
@@ -0,0 +1,53 @@
+__author__ = 'ama6fy'
+
+
+class SpiceDAF(object):
+    def __str__(self):
+        return super().__str__()
+
+    def __init__(self):
+        super().__init__()
+
+
+
+#    ADA      Add data to array
+#    ARR      Add reserved records
+#    ARW      Address to record/word
+#    BBS      Begin backward search
+#    BFS      Begin forward search
+#    BNA      Begin new array
+#    CAD      Continue adding data
+#    CLS      Close
+#    CS       Continue search
+#    ENA      End new array
+#    FNH      File name to handle
+#    FNA      Find next array
+#    FPA      Find previous array
+#    GH       Get handle
+#    GN       Get name
+#    GS       Get summary
+#    HFN      Handle to file name
+#    HSF      Handle to summary format
+#    HLU      Handle to logical unit
+#    HOF      Handles of open files
+#    LUH      Logical unit to handle
+#    NRR      Number of reads, requests
+#    ONW      Open new
+#    OPR      Open for read
+#    OPW      Open for write
+#    PS       Pack summary
+#    RA       Re-order arrays
+#    RCR      Read character record
+#    RDA      Read data from address
+#    RDR      Read double precision record
+#    RFR      Read file record
+#    RN       Replace name
+#    RRR      Remove reserved records
+#    RS       Replace summary
+#    RWA      Record/word to address
+#    SIH      Signal invalid handles
+#    US       Unpack summary
+#    WCR      Write character record
+#    WDA      Write data to address
+#    WDR      Write double precision record
+#    WFR      Write file record
---
+++ SpiceyPy/SpiceErrors.py
@@ -0,0 +1,1 @@
+__author__ = 'ama6fy'
