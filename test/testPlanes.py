4d495a21a3544e9342d2aba82c8d931f5448df4d
Andrew Annex <ama6fy@virginia.edu>
2014-07-07 22:16:00 -0400

    New test meta kernel downloader added, downloads a "minimal" set of kernels for tests. Added "Six" library dependancy for easier Python2/3 conflict fixing. I'm not sure how travis will take this. updated travis file to include six.

--- test/testPlanes.py
+++ test/testPlanes.py
@@ -4,5 +4,5 @@ import SpiceyPy as spice
 testplane = spice.stypes.Plane(tuple([1.0, 20.0, 1.0]), 4.0)
 print(testplane)
 print(spice.pl2nvc(testplane))
-print(spice.stypes.Plane())
-print(spice.nvc2pl([1.0,2.0,3.0], 2.0))
+#print(spice.stypes.Plane())
+#print(spice.nvc2pl([1.0,2.0,3.0], 2.0))
