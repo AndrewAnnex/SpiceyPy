738b18e2f865711b148d6afde4b28105cef7db1e
Andrew Annex <ama6fy@virginia.edu>
2014-07-09 19:51:00 -0400

    try two of getting downloading kernels to work inside setup.py

--- setup.py
+++ setup.py
@@ -21,16 +21,13 @@ data_files = []
 # py.test integration from pytest.org
 class PyTest(TestCommand):
 
-    def __init__(self):
-        super().__init__(self)
-        getTestKernels.downloadKernels()
-
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
 
     def run_tests(self):
+        getTestKernels.downloadKernels()
         import pytest
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
