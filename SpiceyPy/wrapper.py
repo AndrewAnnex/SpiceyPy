4045dfa9773e792189e92c034fa7aee4f79447e1
Andrew Annex <ama6fy@virginia.edu>
2014-09-21 14:46:00 -0400

    this is just a test to see if coveralls python is excluding a wrapper function as specified in a comment next to the function.

--- SpiceyPy/wrapper.py
+++ SpiceyPy/wrapper.py
@@ -1200,8 +1200,8 @@ def ekifld(handle, tabnam, ncols, nrows, cnmlen, cnames, declen, decls):
     handle = ctypes.c_int(handle)
     recptrs = stypes.emptyIntVector(nrows)
     tabnam = stypes.stringToCharP(tabnam)
-    cnames = stypes.listToCharArrayPtr(cnames, xLen=ncols, yLen=cnmlen)
-    decls = stypes.listToCharArrayPtr(decls, xLen=ncols, yLen=declen)
+    cnames = stypes.listToCharArrayPtr(cnames)  # , xLen=ncols, yLen=cnmlen)
+    decls = stypes.listToCharArrayPtr(decls)  #, xLen=ncols, yLen=declen)
     ncols = ctypes.c_int(ncols)
     nrows = ctypes.c_int(nrows)
     cnmlen = ctypes.c_int(cnmlen)
@@ -1634,7 +1634,7 @@ def frmnam(frcode, lenout=125):
     return stypes.toPythonString(frname)
 
 
-def ftncls(unit):
+def ftncls(unit):  # pragma: no cover
     #Todo: close ftncls
     unit = ctypes.c_int(unit)
     libspice.ftncls_c(unit)
