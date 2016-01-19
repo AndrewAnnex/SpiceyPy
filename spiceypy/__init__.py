2b0dbe7ad6fcddc2824136a943c53511425ad30b
AndrewAnnex <ama6fy@virginia.edu>
2016-01-19 00:09:04 -0500

    SpiceyPy-115
    * Attempts to handle default string length args for ease of use

--- spiceypy/__init__.py
+++ spiceypy/__init__.py
@@ -4,6 +4,6 @@ __all__ = ['wrapper']
 
 from spiceypy.wrapper import *
 
-#Default setting for error reporting so that programs don't just exit out!
+# Default setting for error reporting so that programs don't just exit out!
 erract("set", 10, "return")
 errdev("set", 10, "null")
