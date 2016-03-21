1c3de19f1dadd99a3b18a82fcca2364ffc141e4b
AndrewAnnex <ama6fy@virginia.edu>
2016-03-20 20:14:04 -0400

    SpiceyPy-117 Generate Wheel Artifacts in Appveyor Build
    * added wheel artifact generation for Appveyor builds to better support windows.
    * added 3.5 python builds for appveyor.
    * further minimized logging within appveyor.

--- appveyor/makeDynamicSpice.bat
+++ appveyor/makeDynamicSpice.bat
@@ -24,17 +24,17 @@ rem
 rem  Compile everything else.
 rem
 
-for %%f in (*.c) do cl %%f
+for %%f in (*.c) do cl %%f >nul
 
 rem
 rem  Set the cl variable to omit optimization.  Compile zzsecprt.c.
 rem
 
-set cl= /c -D_COMPLEX_DEFINED -DMSDOS -DOMIT_BLANK_CC
+set cl= /c /nologo -D_COMPLEX_DEFINED -DMSDOS -DOMIT_BLANK_CC  >nul
 
 rename zzsecprt.x zzsecprt.c
 
-cl zzsecprt.c
+cl zzsecprt.c >nul
 
 dir /b *.obj > temp.lst
 @echo on
@@ -44,5 +44,6 @@ rem
 rem Create cspice.dll
 rem
 
-link /DLL /OUT:cspice.dll /DEF:cspice.def /IMPLIB:cspice.lib @temp.lst
+link /DLL /OUT:cspice.dll /DEF:cspice.def /IMPLIB:cspice.lib @temp.lst >nul
 @echo on
+rem Finished Linking Spice, makeDynamicSpice.bat completed.
