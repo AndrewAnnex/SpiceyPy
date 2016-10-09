80ddaefd276c9dca8f9ac225bd1a26f9cca9115f
AndrewAnnex <ama6fy@virginia.edu>
2016-10-08 21:34:41 -0400

    making partial fixes

--- appveyor/bld.bat
+++ appveyor/bld.bat
@@ -1,5 +1,17 @@
-IF "%ARCH%"=="32" (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86) ELSE (ECHO "probably a 64bit build")
-IF "%ARCH%"=="64" (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) ELSE (ECHO "probably a 32bit build")
+IF DEFINED DevEnvDir (
+    ECHO "vcvarall.bat has been called"
+) ELSE (
+    IF "%ARCH%"=="32" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+    ) ELSE (
+        ECHO "probably a 64bit build"
+    )
+    IF "%ARCH%"=="64" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+    ) ELSE (
+        ECHO "probably a 32bit build"
+    )
+)
 
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
