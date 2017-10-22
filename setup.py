c8ace7ee18e63c665f9823b3a8341a99e95f162b
Andrew Annex <ama6fy@virginia.edu>
2017-10-22 16:05:15 -0400

    fix to make 3.4 mac builds faster (#221)
    * makes sure pip is updated and that numpy wheel is used for travis builds

--- setup.py
+++ setup.py
@@ -228,6 +228,7 @@ try:
             "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: POSIX :: Linux",
             "Operating System :: Microsoft :: Windows"
