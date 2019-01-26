07f4b57cdbb9f76036b336f95f01a9fd48f3c958
kd7uiy <tuvas21@gmail.com>
2019-01-26 06:54:27 -0500

    Adding unit test for date2et function

--- spiceypy/tests/test_wrapper.py
+++ spiceypy/tests/test_wrapper.py
@@ -27,6 +27,7 @@ import spiceypy as spice
 import numpy as np
 import numpy.testing as npt
 import os
+from datetime import datetime
 
 import spiceypy.utils.callbacks
 from spiceypy.tests.gettestkernels import downloadKernels,\
@@ -7139,6 +7140,23 @@ def test_str2et():
     date = 'Thu Mar 20 12:53:29 PST 1997'
     et = spice.str2et(date)
     npt.assert_almost_equal(et, -87836728.81438904)
+    spice.kclear()
+    
+def test_date2et():
+    spice.kclear()
+    spice.furnsh(CoreKernels.testMetaKernel)
+    date = datetime(1997,3,20,12,53,29)
+    et = spice.str2et(date)
+    npt.assert_almost_equal(et, -87836728.81438904)
+    
+    expecteds=[-87836728.81438904,-792086354.8170365,-790847954.8166842]
+    dates = [datetime(1997,3,20,12,53,29),
+             datetime(1974,11,25,20,0,0),
+             datetime(1974,12,10,04,0,0),
+             
+    results = spice.str2et(dates)
+    for expected, result in zip(expecteds, results)
+        npt.assert_almost_equal(result, expected)
     spice.kclear()
 
 
