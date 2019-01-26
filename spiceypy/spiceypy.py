19f11bcd72e27c7dd247c2fe6b22976bb4d08919
Andrew Annex <ama6fy@virginia.edu>
2019-01-26 15:48:05 -0500

    Update spiceypy/spiceypy.py
    Co-Authored-By: kd7uiy <tuvas21@gmail.com>

--- spiceypy/spiceypy.py
+++ spiceypy/spiceypy.py
@@ -13314,7 +13314,7 @@ def date2et(date):
     """
     Converts a standard Python datetime to a double precision value 
     representing the number of TDB seconds past the J2000 epoch 
-	corresponding to the input epoch.
+    corresponding to the input epoch.
 
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/time.html#The%20J2000%20Epoch
 
