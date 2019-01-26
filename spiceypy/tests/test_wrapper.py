501ffba78e05fcb39d35ee56f7a2b248f2952194
kd7uiy <tuvas21@gmail.com>
2019-01-26 09:18:48 -0500

    Fixed expected result
    Time zones were a factor with the previous value, but they aren't involved in Python datetimes. Changed to correct for time zone issue

--- spiceypy/tests/test_wrapper.py
+++ spiceypy/tests/test_wrapper.py
@@ -7147,9 +7147,9 @@ def test_date2et():
     spice.furnsh(CoreKernels.testMetaKernel)
     date = datetime(1997,3,20,12,53,29)
     et = spice.date2et(date)
-    npt.assert_almost_equal(et, -87836728.81438904)
+    npt.assert_almost_equal(et, -87865528.8143913)
     
-    expecteds=[-87836728.81438904,-792086354.8170365,-790847954.8166842]
+    expecteds=[-87865528.8143913,-792086354.8170365,-790847954.8166842]
     dates = [datetime(1997,3,20,12,53,29),
              datetime(1974,11,25,20,0,0),
              datetime(1974,12,10,4,0,0)]
