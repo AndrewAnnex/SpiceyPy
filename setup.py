a12c9053d32db5f7839b621960d533c225e9e799
AndrewAnnex <ama6fy@virginia.edu>
2019-08-04 01:38:15 -0700

    just updating setup py for old python

--- setup.py
+++ setup.py
@@ -46,9 +46,9 @@ cspice_dir = os.path.join(root_dir, 'cspice')
 # Make the directory path for cspice/lib
 lib_dir = os.path.join(cspice_dir, 'lib')
 
-TEST_DEPENDENCIES = ['numpy>=1.12.0', 'six>=1.9.0', 'pytest>=2.9.0']
-DEPENDENCIES = ['numpy>=1.12.0', 'six>=1.9.0', 'certifi>=2017.1.23']
-REQUIRES = ['numpy', 'six']
+TEST_DEPENDENCIES = ['numpy<=1.16.4;python_version<"3.3"', 'numpy>=1.17.0;python_version>="3.3"', 'six>=1.9.0', 'pytest>=2.9.0']
+DEPENDENCIES = ['numpy<=1.16.4;python_version<"3.3"', 'numpy>=1.17.0;python_version>="3.3"', 'six>=1.9.0', 'certifi>=2017.1.23']
+REQUIRES = ['numpy<=1.16.4;python_version<"3.3"', 'numpy>=1.17.0;python_version>="3.3"', 'six>=1.9.0']
 
 # If we have an old version of OpenSSL, CSPICE will be downloaded
 # (if required) using urllib3.  Extend the list of required packages.
