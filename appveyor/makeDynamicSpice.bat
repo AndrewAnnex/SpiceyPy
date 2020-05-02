62a475a886349ea70346f7948c6fe269622c3cec
Andrew Annex <ama6fy@virginia.edu>
2020-05-01 21:02:37 -0400

    double checking that this is building 64 bit cspice

--- appveyor/makeDynamicSpice.bat
+++ appveyor/makeDynamicSpice.bat
@@ -44,6 +44,6 @@ rem
 rem Create cspice.dll
 rem
 
-link /DLL /OUT:cspice.dll /DEF:cspice.def /IMPLIB:cspice.lib @temp.lst >nul
+link /MACHINE:X64 /DLL /OUT:cspice.dll /DEF:cspice.def /IMPLIB:cspice.lib @temp.lst >nul
 @echo on
 rem Finished Linking Spice, makeDynamicSpice.bat completed.
