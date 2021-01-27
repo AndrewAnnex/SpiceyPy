98390927be0673ff31a2374217adc433354289e5
Andrew Annex <ama6fy@virginia.edu>
2021-01-27 10:20:23 -0500

    Update conf.py
    update copyright date for docs

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "SpiceyPy"
-copyright = "2014-2020, Andrew Annex"
+copyright = "2014-2021, Andrew Annex"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
