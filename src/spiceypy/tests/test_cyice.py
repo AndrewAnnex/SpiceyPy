403e31bb8140c248bc6436d53b049d6b71cfae05
Andrew Annex <ama6fy@virginia.edu>
2023-01-15 13:05:30 -0800

    working tests

--- src/spiceypy/tests/test_cyice.py
+++ src/spiceypy/tests/test_cyice.py
@@ -1,6 +1,65 @@
+import os
+
 import pytest
-from spiceypy.cyice.cyice import cyice
+
+from spiceypy.tests.gettestkernels import (
+    download_kernels,
+    CoreKernels,
+    CassiniKernels,
+    ExtraKernels,
+    cleanup_cassini_kernels,
+    cleanup_extra_kernels,
+    cleanup_core_kernels,
+    cwd,
+)
+import numpy as np
+import numpy.testing as npt
+import spiceypy as spice
+from spiceypy.cyice import cyice
+
+
+@pytest.fixture(autouse=True)
+def clear_kernel_pool_and_reset():
+    spice.kclear()
+    spice.reset()
+    # yield for test
+    yield
+    # clear kernel pool again
+    spice.kclear()
+    spice.reset()
+
+
+def cleanup_kernel(path):
+    spice.kclear()
+    spice.reset()
+    if spice.exists(path):
+        os.remove(path)  # pragma: no cover
+    pass
+
+
+def setup_module(module):
+    download_kernels()
 
 
 def test_b1900():
     assert cyice.b1900() == 2415020.31352
+
+
+def test_spkezr_vectorized():
+    spice.furnsh(CoreKernels.testMetaKernel)
+    et = np.full((100,), spice.str2et("July 4, 2003 11:00 AM PST"))
+    state, lt = cyice.spkezr_vectorized("Mars", et, "J2000", "LT+S", "Earth")
+    expected_lt = np.full((100,), 269.6898816177049)
+    expected_state = np.full(
+        (100, 6),
+        [
+            73822235.33116072,
+            -27127919.178592984,
+            -18741306.284863796,
+            -6.808513317178952,
+            7.513996167680786,
+            3.001298515816776,
+        ],
+    )
+    npt.assert_allclose(lt, expected_lt)
+    npt.assert_allclose(state, expected_state)
