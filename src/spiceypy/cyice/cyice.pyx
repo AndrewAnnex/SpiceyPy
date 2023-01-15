b1b9862a3cc4d5cb780f01b4e1372c6aaa1fb281
Andrew Annex <ama6fy@virginia.edu>
2023-01-14 23:48:05 -0800

    added cells and a functions to extern, no wrappers yet

--- src/spiceypy/cyice/cyice.pyx
+++ src/spiceypy/cyice/cyice.pyx
@@ -10,7 +10,7 @@ cpdefs are both, really only useful for recursion
 
 # okay for some reason making a pxd file causes cython to fail
 # todo maybe okay to just use "*" for header file
-# grab externs from SpiceUsr.h
+# grab externs from SpiceZpr.h
 cdef extern from "SpiceUsr.h" nogil:
     ctypedef int SpiceBoolean
     ctypedef char SpiceChar
@@ -20,6 +20,63 @@ cdef extern from "SpiceUsr.h" nogil:
     ctypedef const char ConstSpiceChar
     ctypedef const long ConstSpiceInt
     ctypedef const double ConstSpiceDouble
+    
+    # Cells
+    cdef enum SpiceCellDataType:
+        char   = 0,
+        double = 1,
+        int    = 2,
+        time   = 3,
+        bool   = 4
+
+    cdef struct _SpiceCell:
+        SpiceCellDataType dtype
+        SpiceInt           length
+        SpiceInt           size
+        SpiceInt           card
+        SpiceBoolean       isSet
+        SpiceBoolean       adjust
+        SpiceBoolean       init
+        void * base
+        void * data
+    
+    ctypedef _SpiceCell SpiceCell
+    ctypedef const SpiceCell ConstSpiceCell
+
+    # start of function defs
+    # A
+    cdef void appndc_c(ConstSpiceChar * item,
+                       SpiceCell * cell);
+
+    cdef void appndd_c(SpiceDouble item,
+                       SpiceCell * cell);
+
+    cdef void appndi_c(SpiceInt item,
+                       SpiceCell * cell);
+
+    cdef void axisar_c(ConstSpiceDouble     axis   [3],
+                       SpiceDouble          angle,
+                       SpiceDouble          r      [3][3]);
+
+    cdef void azlcpo_c(ConstSpiceChar * method,
+                       ConstSpiceChar * target,
+                       SpiceDouble         et,
+                       ConstSpiceChar * abcorr,
+                       SpiceBoolean        azccw,
+                       SpiceBoolean        elplsz,
+                       ConstSpiceDouble    obspos [3],
+                       ConstSpiceChar * obsctr,
+                       ConstSpiceChar * obsref,
+                       SpiceDouble         azlsta [6],
+                       SpiceDouble * lt);
+
+    cdef void  azlrec_c(SpiceDouble         range,
+                        SpiceDouble         az,
+                        SpiceDouble         el,
+                        SpiceBoolean        azccw,
+                        SpiceBoolean        elplsz,
+                        SpiceDouble         rectan [3]);
+    # B
     cdef double b1900_c()
 
 cpdef b1900():
